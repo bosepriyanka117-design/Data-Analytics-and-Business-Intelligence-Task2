5765d32d5c994a68dee7feaaac26949d97415c02
bosepriyanka117-design <bosepriyanka117@gmail.com>
2026-06-25 13:37:53 +0530

    Add files via upload

---
+++ Task2.sql
@@ -0,0 +1,8 @@
+create database Task2;
+use task2;
+CREATE TABLE Customers (
+    Customer_ID VARCHAR(50) PRIMARY KEY,
+    Customer_Name VARCHAR(150) NOT NULL,
+    Region VARCHAR(100),
+    Segment VARCHAR(100)
+);
